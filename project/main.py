1324349d7f661cd0d017270997db41a4b4e23d15
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2020-11-29 20:58:28 +0900

    학점평균

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
-cuss_list = []
+import sys
 
-
-def average():
+# 맨 처음 보이는 화면
+def messege():
     print(
         "",
         "---------------------",
@@ -13,81 +13,97 @@ def average():
         sep="\n"
     )
 
-
-
-
-
-while True:
-    average()
+# 과목 수
+def subJect_Nums():
+    subJect_Nums = int(input("수강한 과목의 수를 입력해주세요: "))
+    return subJect_Nums
+ 
+# 과목 수 for함수
+def Sum_Scores(Subject_Nums):
+ 
+    Sum_Score = 0
+    for i in range(Subject_Nums):
+        message = str(i+1) + "번째 과목의 점수를 입력하세요: "
+        score = int(input(message))
+        Sum_Score = Sum_Score + score
+    return Sum_Score
+ 
+# 점수 학점 치환
+def Exam_Score(Score):
+    grade = 'F'
+    if Score >=4.5:
+        grade = 'A+'
+    elif Score >= 4.0:
+        grade = 'A'
+    elif Score >= 3.5:
+        grade = 'B+'
+    elif Score >= 3.0:
+        grade = 'B'
+    elif Score >= 2.5:
+        grade = 'C+'
+    elif Score >= 2.0:
+        grade = 'C'
+    elif Score >= 1.5:
+        grade = 'D+'
+    elif Score >= 1.0:
+        grade = 'D'
+    else:
+        grade ='F'
+    print(grade)
+ 
+# 전체 나누기 스코어
+def get_average_score(Score, Subject_Nums):
+    result = float(Score) / Subject_Nums 
+    return result
+ 
+ 
+# 메인함수
+def main():
+    messege()
     choice = input("실행하고자 하는 번호를 입력해주세요 >>")
     if choice == "0":
         print("프로그램을 종료합니다.")
-        break
+    
     if choice == "1":
-        grade = []
-        print("학점을 입력해 주세요",
-              "A+, A, B+, B, C+ C, D+, D, F",
-              "4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, Fail      ETC...",
-              sep="\n")
-
-      
-        score_Aplus = input("점수를 입력해 주세요")
-        print(score_Aplus)
-        score_A = input("점수를 입력해 주세요")
-        print(score_A)
-        score_Bplus = input("점수를 입력해 주세요")
-        print(score_Bplus)
-        score_B = input("점수를 입력해 주세요")
-        print(score_B)
-        score_Cplus = input("점수를 입력해 주세요")
-        print(score_Cplus)
-        score_Dplus = input("점수를 입력해 주세요")
-        print(score_Dplus)
-        score_D = input("점수를 입력해 주세요")
-        print(score_D)
-        score_F = input("점수를 입력해 주세요")
-        print(score_F)
-        grade.push(score)
-        print(score)
-
-        if score_Aplus == "A+":
-            score = 4.5
-            print(score_Aplus)
-        elif score_A == "A":
-            score = 4
-            print(score_A)
-        elif score_Bplus == "B+":
-            score = 3.5
-            print(score_Bplus)
-        elif score_B == "B":
-            score = 3
-            print(score_B)
-        elif score_Cplus == "C+":
-            score = 2.5
-            print(score_Cplus)
-        elif score_C == "C":
-            score = 2
-            print(score_C)
-        elif score_F == "F":
-            score = 'Fail'
-            print(score_F)
-
-        break
-
+        number_of_subjects = subJect_Nums()
+        total_score = Sum_Scores(number_of_subjects)
+        average_score = get_average_score(total_score, number_of_subjects)
+        Exam_Score(average_score)
     if choice == "2":
         subjects = ['프로그래밍','python','C','C++','C#']
-        print("들어야 하는 과목을 보여드립니다.", subjects,
-              sep="\n")
-
-        i = 0
-        while i < 5:
-            i = i + 1
-            fir_Subject = input("들었던 과목을 넣어주세요.종료는 0번 입니다.")
-            subjects.remove(fir_Subject)
+        print(
+            "",
+            '----------------------',
+            '1 : 수강 과목 입력',
+            '2 : 수강 과목 삭제',
+            '3 : 수강 중인 과목',
+            '4 : 하나씩 과목 삭제',
+            '----------------------',
+            '',
+            sep = '\n'
+        )
+        sub_choice = input('실행 할 번호를 입력해 주세요')
+        if sub_choice == '1' :
+            in_subject = input('리스트 안에 넣을 과목을 입력해주세요')
+            subjects.append(in_subject)
+        elif sub_choice == '2':
+            del_subject = input('삭제할 과목을 입력해 주세요')
+            subjects.remove(del_subject)
+        elif sub_choice == '3':
             print(subjects)
+        elif sub_choice == '4':
+            print("들어야 하는 과목을 보여드립니다.", subjects,
+              sep="\n")
+            i = 0
+            while i < len(subjects):
+                fir_Subject = input("들었던 과목을 넣어주세요.")
+                subjects.remove(fir_Subject)
+                print(subjects)
+                print(len(subjects))
 
-            if i == 5:
-                print("모든 과목을 수강하셨습니다.")
-                break
+                if i == len(subjects):
+                    print("모든 과목을 수강하셨습니다.")
+                    break
 
-
+if __name__ == '__main__':
+    main()
